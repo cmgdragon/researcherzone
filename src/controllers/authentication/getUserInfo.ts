b19b6f4e08a268fe2e1cadda31a01844d0b559a6
cmgdragon <52174497+cmgdragon@users.noreply.github.com>
2021-05-31 13:08:19 +0200

    removed unused condition

--- src/controllers/authentication/getUserInfo.ts
+++ src/controllers/authentication/getUserInfo.ts
@@ -1,6 +1,6 @@
 import { Context } from "oak";
 import { decode } from 'djwt';
-import { findUserByEmail, findUserById } from '~/controllers/database/users.ts';
+import { findUserByEmail } from '~/controllers/database/users.ts';
 import { findDocuments } from '~/controllers/database/documents.ts';
 import { Bson } from 'deno-mongo';
 import cookie from 'cookie';
@@ -9,46 +9,23 @@ const getUserInfo = async context => {
 
     const path = context.request.headers.get('pathname');
 
-    if (path.includes('/user')) {
+    const { token } = cookie.parse(context.request.headers.get('cookie') || '');
+    const [ header, payload, signature ] : any = decode(token);
+    try {
 
-        const segments = path.split('/');
+        const user = await findUserByEmail(payload.iss.email);
+        const documents = await findDocuments(payload.iss.email);
 
-        try {
-            const user = await findUserById(new Bson.ObjectID(segments[segments.length-1]));
-            const documents = await findDocuments(user.email);
-            context.response.body = { 
-                isGuest: true,
-                user: {...user, pwd: '', email: ''},
-                documents
-            };
-        } catch (error) {
-            context.response.body = { 
-                message: error.message
-            };
-        }
+        context.response.body = { 
+            isGuest: false,
+            user: {...user, pwd: ''},
+            documents
+        };
 
-
-    } else {
-
-        const { token } = cookie.parse(context.request.headers.get('cookie') || '');
-        const [ header, payload, signature ] : any = decode(token);
-        try {
-
-            const user = await findUserByEmail(payload.iss.email);
-            const documents = await findDocuments(payload.iss.email);
-    
-            context.response.body = { 
-                isGuest: false,
-                user: {...user, pwd: ''},
-                documents
-            };
-
-        } catch (error) {
-            context.response.body = { 
-                message: error.message
-            };
-        }
-
+    } catch (error) {
+        context.response.body = { 
+            message: error.message
+        };
     }
 
 }
